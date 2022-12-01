9a9e3fce6c56dd77d82006efe0b3e34c19e91185
Padinn21 <alifizudin7758@gmail.com>
2022-12-02 01:01:23 +0700

    layout home;search;result;detail

---
+++ app/src/main/java/com/binarfp/airtrip/presentation/ui/buyer/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.binarfp.airtrip.presentation.ui.buyer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.binarfp.airtrip.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
